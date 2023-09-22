1dc84fbba7c4908984275b1b31cadf7fe2f71b85
Namo333 <dad688634@gmail.com>
2023-09-22 21:47:11 +0300

    add map

--- frontend/src/lib/components/map.svelte
+++ frontend/src/lib/components/map.svelte
@@ -1,9 +1,25 @@
 <script>
-
-</script>
+    import { onMount } from 'svelte';
+  
+    let mapContainer;
+  
+    onMount(() => {
+      if (typeof window !== 'undefined') {
+        // Мы на клиенте, можете импортировать Leaflet и инициализировать карту
+        import('leaflet').then((L) => {
+          const map = L.map(mapContainer).setView([51.505, -0.09], 13);
+  
+          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+          }).addTo(map);
+        });
+      }
+    });
+  </script>
 
 <main class="map__content">
-
+    <div bind:this="{mapContainer}" id="mapContainer"></div>
 </main>
 
 <style>
@@ -13,5 +29,9 @@
         border-radius: 40px;
         background: var(--Gradient-blue, linear-gradient(233deg, #9FCCFA -20.48%, #0974F1 97.38%));
         flex-shrink: 0;
+    }
+    #mapContainer {
+        width: 200px;
+        height: 400px;
     }
 </style>
